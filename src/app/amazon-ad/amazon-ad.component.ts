003b261bc75c8e717a36c792ef8b89a32b0a3614
Steve Sanders <sdsanders1@gmail.com>
2017-10-14 12:02:56 -0500

    Allow for multiple amazon ad ids

--- src/app/amazon-ad/amazon-ad.component.ts
+++ src/app/amazon-ad/amazon-ad.component.ts
@@ -5,18 +5,20 @@ import { Component, Input, Renderer2, OnInit, ElementRef } from '@angular/core';
   template: ''
 })
 export class AmazonAdComponent implements OnInit {
+  @Input('ad') ad: string;
   constructor(
     private renderer: Renderer2,
     private el: ElementRef
   ) {}
 
   public ngOnInit() {
+    console.log(this.ad);
     const s = this.renderer.createElement('script');
 
     this.renderer.setAttribute(
       s,
       'src',
-      '//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=eff08152-9faf-4f4e-94a7-14ba379d3517'
+      `//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=${this.ad}`
     );
 
     this.renderer.appendChild(this.el.nativeElement, s);
