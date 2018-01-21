1b5664ceee01e65d0a4c967e17cc8e582d549e1a
Steve Sanders <sdsanders1@gmail.com>
2018-01-20 18:11:36 -0600

    Update home page meta description

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, ViewChild, AfterViewInit, Renderer } from '@angular/core';
+import { Component, AfterViewInit, Renderer } from '@angular/core';
 import { MetaService } from '../meta.service';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html'
 })
-export class HomePageComponent {
+export class HomePageComponent implements AfterViewInit {
 
   constructor(
     public renderer: Renderer,
@@ -16,7 +16,7 @@ export class HomePageComponent {
     this.meta.setTitle(this.renderer, 'episode.ninja | The Best Episodes of Your Favorite Shows');
     this.meta.addTag(this.renderer, {
       name: 'description',
-      content: 'The highest user rated episodes of any show'
+      content: 'The best episodes of any television show chosen by millions of ratings from fans!'
     });
   }
 
