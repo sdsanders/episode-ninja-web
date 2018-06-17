1de2be0d95ddb8643cf229da9a3065d45741b7dd
Steve Sanders <sdsanders1@gmail.com>
2018-06-17 14:12:04 -0500

    Point to new API url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://episodes.stevendsanders.com'
+  apiUrl: 'https://api.episode.ninja'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://episodes.stevendsanders.com'
+  apiUrl: 'https://api.episode.ninja'
 };
