88dc3c432f4cd522a240cb8d7d97cb9bd7545f27
Steve Sanders <sdsanders1@gmail.com>
2019-02-24 20:41:32 -0600

    Fix search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
 export class SearchComponent {
   public query: String = '';
 
-  constructor() {}
+  constructor(private router: Router) {}
+
+  onSubmit() {
+    this.router.navigate(['/search'], { queryParams: { query: this.query } });
+  }
 }
