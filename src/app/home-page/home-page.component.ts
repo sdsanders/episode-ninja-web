a99da1a1f9b815478bca734e0c3c21f63b8cec6d
Steve Sanders <sdsanders1@gmail.com>
2019-04-07 19:00:43 -0500

    Add sharing buttons to home page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -3,7 +3,8 @@ import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home-page',
-  templateUrl: './home-page.component.html'
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
 
