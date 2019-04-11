c4e9f3c78fd2ae79f329c8b1e87484dcba20d2fe
Steve Sanders <sdsanders1@gmail.com>
2019-04-10 22:41:50 -0500

    Adjust dev api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:4000',
+  apiUrl: 'https://api.episode.ninja',
   amplify: {
     Auth: {
       region: 'us-east-1',
