55ecb410aa681f7f39de1c8e8b45276d37832d3a
Steve Sanders <sdsanders1@gmail.com>
2019-08-20 18:15:22 -0500

    Improve search results styling

--- src/app/show-card/show-card.component.ts
+++ src/app/show-card/show-card.component.ts
@@ -2,7 +2,8 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-show-card',
-  templateUrl: './show-card.component.html'
+  templateUrl: './show-card.component.html',
+  styleUrls: ['./show-card.component.scss']
 })
 export class ShowCardComponent {
   @Input() show;
