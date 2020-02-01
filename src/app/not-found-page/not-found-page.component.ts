8e64cd020ea1fd1c7d8c48ebcff5c329df935cc2
Steve Sanders <sdsanders1@gmail.com>
2020-02-01 13:37:02 -0600

    Upgrade to Angular 9 and static prerendering

--- src/app/not-found-page/not-found-page.component.ts
+++ src/app/not-found-page/not-found-page.component.ts
@@ -1,23 +1,10 @@
-import { Component, OnInit, Optional, Inject, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
-import { RESPONSE } from '@nguniversal/express-engine/tokens';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-not-found-page',
   templateUrl: './not-found-page.component.html',
   styleUrls: ['./not-found-page.component.css']
 })
-export class NotFoundPageComponent implements OnInit {
-
-  constructor(
-    @Optional() @Inject(RESPONSE) private response: any,
-    @Inject(PLATFORM_ID) private platformId: Object
-  ) {}
-
-  ngOnInit() {
-    if (!isPlatformBrowser(this.platformId)) {
-      this.response.status(404);
-    }
-  }
+export class NotFoundPageComponent {
 
 }
